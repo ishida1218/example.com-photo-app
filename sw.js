bc7d5d344bf03971c7993bb260d5c5391f0e98a9
ishida1218 <ishida@thc.co.jp>
2025-04-04 16:30:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('offline-cache').then(cache => {
-        return cache.addAll([
-          './',
-          './index.html',
-          './manifest.json'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = "image-save-pwa-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// インストール時にキャッシュ
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// リクエスト時にキャッシュ優先で返す
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
